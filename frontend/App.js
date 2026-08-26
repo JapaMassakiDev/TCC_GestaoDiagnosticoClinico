55c2713827b89f4fe34ffdde7c70f6aed877dd2c
Gabriel 1905 <gabrielmuemura@gmail.com>
2026-08-26 03:20:23 -0300

    creating cadaster and login

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,72 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { AuthProvider } from './src/context/AuthContext';
-import { AppNavigator } from './src/navigation/AppNavigator';
+import { useState } from "react";
+
+import "./global.css";
+
+import LoginScreen from "./src/screens/LoginScreen.js";
+import CadastroScreen from "./src/screens/CadastroScreen";
 
 export default function App() {
+  const [tela, setTela] = useState("login");
+
+  const [cpfParaCadastro, setCpfParaCadastro] =
+    useState("");
+
+  function abrirCadastro(cpf) {
+    setCpfParaCadastro(cpf || "");
+    setTela("cadastro");
+  }
+
+  function abrirLogin() {
+    setCpfParaCadastro("");
+    setTela("login");
+  }
+
+  function loginSucesso(usuario) {
+    console.log(
+      "Usuário autenticado:",
+      usuario
+    );
+
+    /*
+      Aqui posteriormente você pode:
+
+      - salvar o token
+      - abrir a Home
+      - usar Context API
+      - usar AsyncStorage
+      - carregar os dados do usuário
+    */
+  }
+
+  function cadastroSucesso(usuario) {
+    console.log(
+      "Usuário cadastrado:",
+      usuario
+    );
+
+    /*
+      Depois do cadastro você pode
+      automaticamente autenticar o usuário
+      ou voltar para o login.
+    */
+
+    setTela("login");
+  }
+
+  if (tela === "cadastro") {
+    return (
+      <CadastroScreen
+        cpfInicial={cpfParaCadastro}
+        onVoltarLogin={abrirLogin}
+        onCadastroSucesso={cadastroSucesso}
+      />
+    );
+  }
+
   return (
-    <AuthProvider>
-      <AppNavigator />
-      <StatusBar style="dark" />
-    </AuthProvider>
+    <LoginScreen
+      onIrParaCadastro={abrirCadastro}
+      onLoginSucesso={loginSucesso}
+    />
   );
 }
